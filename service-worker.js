6a50b4547a9bec0785c17eff073bf0070e7e9f5d
admintelecomgroup-cmd <admintelecomgroup@gmail.com>
2026-08-08 14:52:08 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,54 +1,39 @@
-// ==========================================
-// Telecom Group Chattogram City
-// Service Worker
-// ==========================================
-
-const CACHE_NAME = "telecom-group-v3";
+const CACHE_NAME = "telecom-group-v4";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./admin.html",
-  "./login.html",
-  "./style.css",
-  "./script.js",
-  "./supabase.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./supabase.js",
+    "./manifest.json"
 ];
 
-// Install
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
 });
 
-// Activate
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        ).then(() => self.clients.claim())
+    );
 });
 
-// Fetch
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
