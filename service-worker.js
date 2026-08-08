b75d454e192afab8add3e4a0e67ada8d74713b70
admintelecomgroup-cmd <admintelecomgroup@gmail.com>
2026-08-08 13:29:11 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Service Worker
 // ==========================================
 
-const CACHE_NAME = "telecom-group-v2";
+const CACHE_NAME = "telecom-group-v3";
 
 const FILES_TO_CACHE = [
   "./",
